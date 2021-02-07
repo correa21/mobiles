ef111d562e23e62638164e31e4a4f0cb98d65b9d
Armando Correa <ie708826@iteso.mx>
2021-02-07 16:13:27 -0600

    feat(examen1): adds hotdrinks list view

--- examen/estructura_practica_uno/lib/drinks/item_hot_drinks.dart
+++ examen/estructura_practica_uno/lib/drinks/item_hot_drinks.dart
@@ -15,6 +15,74 @@ class ItemHotDrinks extends StatefulWidget {
 class _ItemHotDrinksState extends State<ItemHotDrinks> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("${widget.drink.productTitle}"));
+    return Container(
+      height: 220,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            top: 16,
+            child: Card(
+              elevation: 4.0,
+              margin: EdgeInsets.all(24.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              color: Colors.indigo,
+              child: Container(height: 180),
+            ),
+          ),
+          Container(
+            height: 160,
+            margin: EdgeInsets.only(top: 28, bottom: 24, left: 24, right: 24),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.all(
+                Radius.circular(30),
+              ),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 16),
+                  width: 160,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow[300],
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Text(
+                      "${widget.drink.productTitle}",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(5.0),
+                bottomRight: Radius.circular(5.0),
+              ),
+              child: Image.network(
+                "${widget.drink.productImage}",
+                fit: BoxFit.contain,
+                height: 180,
+                width: 180,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- examen/estructura_practica_uno/lib/home/home.dart
+++ examen/estructura_practica_uno/lib/home/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:estructura_practica_1/home/item_home.dart';
 import 'package:estructura_practica_1/profile.dart';
+import 'package:estructura_practica_1/drinks/hot_drinks_page.dart';
 
+import '../models/product_repository.dart';
 import '../profile.dart';
 
 class Home extends StatefulWidget {
@@ -24,13 +26,16 @@ class _HomeState extends State<Home> {
           GestureDetector(
             onTap: _openHotDrinksPage,
             child: ItemHome(
-              title: "Bebidas caliente",
+              title: "Bebidas calientes",
               image: "https://i.imgur.com/XJ0y9qs.png",
             ),
           ),
-          ItemHome(
-            title: "Postres",
-            image: "https://i.imgur.com/fI7Tezv.png",
+          GestureDetector(
+            onTap: _openDessertPage,
+            child: ItemHome(
+              title: "Postres",
+              image: "https://i.imgur.com/fI7Tezv.png",
+            ),
           ),
           ItemHome(
             title: "Granos",
@@ -54,10 +59,8 @@ class _HomeState extends State<Home> {
 
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) {
-          return null;
-        },
-      ),
+          builder: (context) => HotDrinksPage(
+              drinksList: ProductRepository.loadProducts(ProductType.BEBIDAS))),
     );
   }
 
